6b3fd6620ac1eca6fe2523a7ac338d6de562a3f3
stas <spert>
2026-04-26 01:43:52 +0300

    f

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,22 @@
-import type { NextConfig } from "next";
+// next.config.mjs
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+    dest: "public",         // Где будет лежать sw.js
+    cacheOnFrontEndNav: true, // Кешировать страницы при переходах
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,    // Обновить страницу при появлении сети
+    disable: process.env.NODE_ENV === "development", // Отключаем в деве
+});
+
+export default withPWA({
+    // Твои обычные настройки Next.js 16
+});
+
+
+
+/* import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {};
 
-export default nextConfig;
+export default nextConfig; */
